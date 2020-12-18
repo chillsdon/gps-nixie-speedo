ec5721d174ff066f280e4de222125348334c9e2c
Craig <chillsdon@gmail.com>
2020-12-18 20:07:00 +0000

    Converting for shift registers

--- gps-nixie-speedo.ino
+++ gps-nixie-speedo.ino
@@ -4,7 +4,11 @@
 static const int RXPin = 8, TXPin = 9;
 static const int GPSBaud = 9600;
 
-int speed = 0;
+static const int _latchPin = 2;
+static const int _clockPin = 1;
+static const int _dataPin = 3;
+
+int speedValue = 0;
 int lastSpeed = -1;
 char data[100];
 bool sendOutput;
@@ -14,12 +18,22 @@ TinyGPSPlus gps;
 
 void setup()
 {
-  Serial.begin(9600);
-  ss.begin(GPSBaud);
+  //Serial.begin(9600);
+  //ss.begin(GPSBaud);
 }
 
 void loop()
 {
+  displayCount();
+
+  delay(250);
+
+  speedValue =+1;
+
+  if (speedValue == 10)
+    speedValue = 0;
+
+  /*
   smartDelay(50);  
 
   //Serial.println(F("Hello..."));
@@ -45,6 +59,52 @@ void loop()
   {
     Serial.println(F("No GPS detected"));
   }
+  */
+}
+
+void displayCount()
+{
+  int lower;
+  int upper;
+
+  if (speedValue >= 100)
+  {
+    upper = 1;
+    lower = speedValue - 100;
+  }
+  else
+  {
+    upper = 0;
+    lower = speedValue;
+  }
+
+  if (visible)
+  {
+    
+  }
+  else
+  {
+    
+  }
+
+  byte lowerByte = encodeNumber(lower);
+  byte upperByte = encodeNumber(upper);
+
+  digitalWrite(_latchPin, LOW);
+  shiftOut(_dataPin, _clockPin, MSBFIRST, lowerByte);
+  shiftOut(_dataPin, _clockPin, MSBFIRST, upperByte);
+  digitalWrite(_latchPin, HIGH);
+}
+
+byte encodeNumber(int num)
+{
+  byte tens = num / 10;
+  byte units = num - (tens * 10);
+
+  byte data = (units << 4) | tens; 
+
+  return
+    data;
 }
 
 // This custom version of delay() ensures that the gps object
