af6055ac485c3ed1e6976853d5e16df798d744f0
Craig <chillsdon@gmail.com>
2020-12-23 20:45:48 +0000

    More testing

--- gps-nixie-speedo.ino
+++ gps-nixie-speedo.ino
@@ -30,18 +30,33 @@ void setup()
 
 void loop()
 {
-  byte a = 1;
-  byte b = 2;
-  byte c = 3;
-  byte d = 4;
+  //byte a = speedValue;
+  //byte b = speedValue;
+  //byte c = speedValue;
+  //byte d = speedValue;
 
-  byte first = (a << 4) | b;
-  byte last = (c << 4) | d;
+  byte enable = 0;
+  byte hundreds = speedValue / 100;
+  byte tens = (speedValue - (hundreds * 100)) / 10;
+  byte units = speedValue - (hundreds * 100) - (tens * 10);
+
+  //byte first = (a << 4) | b;
+  //byte last = (c << 4) | d;
+
+  byte first = (units << 4) | tens;
+  byte last = (hundreds << 4) | enable;
 
   digitalWrite(_latchPin, LOW);
   shiftOut(_dataPin, _clockPin, MSBFIRST, first);
   shiftOut(_dataPin, _clockPin, MSBFIRST, last);
   digitalWrite(_latchPin, HIGH);
+
+  speedValue += 1;
+
+  if (speedValue > 999)
+    speedValue = 0;
+
+  delay(250);
   
   /*
   Serial.print("speedValue: ");
@@ -84,51 +99,6 @@ void loop()
     Serial.println(F("No GPS detected"));
   }
   */
-}
-
-void displayCount()
-{
-  encodeSpeed(speedValue);
-
-  //speedBytes[0] = (2 << 4) | 0;
-  //speedBytes[1] = (0 << 4) | 0;
-
-  digitalWrite(_latchPin, LOW);
-  shiftOut(_dataPin, _clockPin, MSBFIRST, speedBytes[0]);
-  shiftOut(_dataPin, _clockPin, MSBFIRST, speedBytes[1]);
-  digitalWrite(_latchPin, HIGH);
-}
-
-void encodeSpeed(int value)
-{
-  byte enable = 0;
-  
-  byte hundreds = value / 100;
-  byte tens = (value - (hundreds * 100)) / 10;
-  byte units = value - (hundreds * 100) - (tens * 10);
-
-  if (hundreds > 0)
-    enable = enable | 1;
-
-  if (tens > 0)
-    enable = enable | 2;
-
-  enable = 15;
-
-  //if (units % 2 == 0)
-  //  enable = 1;
-
-  Serial.print("Value: ");
-  Serial.println(value);
-  Serial.print("Hundreds: ");
-  Serial.println(hundreds);
-  Serial.print("Tens: ");
-  Serial.println(tens);
-  Serial.print("Units: ");
-  Serial.println(units);
-
-  speedBytes[0] = (units << 4) | tens;
-  speedBytes[1] = (hundreds << 4) | enable;
 }
 
 // This custom version of delay() ensures that the gps object
